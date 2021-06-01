71c4261c306778f822a48703c8032c5cb0037111
blayziken <kingtoluwalope@gmail.com>
2021-06-01 14:27:04 +0100

    added implementation to delete item from basket

--- lib/provider/Basket.dart
+++ lib/provider/Basket.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 
 class BasketItem {
+  final String id;
   final String shop;
   final String foodName;
   final String description;
@@ -10,6 +11,7 @@ class BasketItem {
   final String foodOrSnacks;
 
   BasketItem({
+    this.id,
     this.foodOrSnacks,
     this.imageName,
     this.foodName,
@@ -21,7 +23,7 @@ class BasketItem {
 }
 
 class Basket with ChangeNotifier {
-  List<BasketItem> _basketList = [
+  List<BasketItem> _basketItems = [
 //    BasketItemTile(
 //        shopName: 'Mavis Campus',
 //        description: 'Scaling up satisfaction',
@@ -36,22 +38,22 @@ class Basket with ChangeNotifier {
 //        price: 'N600'),
   ];
 
-  List<BasketItem> get basketList {
-    return [..._basketList];
+  // GET BASKET ITEMS
+  List<BasketItem> get basketItems {
+    return [..._basketItems];
   }
 
+  // GET LENGTH OF BASKET ITEMS
+  int getLength() {
+    return _basketItems.length;
+  }
+
+  // ADD ITEM TO BASKET
   void addToBasket(BasketItem basketItem) {
-//    _basketList.add(
-//      BasketItemTile(
-//        foodName: basketItem.foodName,
-//        shopName: basketItem.shop,
-//        price: basketItem.price,
-//      ),
-//    );
-    //
-    _basketList.insert(
+    _basketItems.insert(
       0,
       BasketItem(
+        id: DateTime.now().toString(),
         foodName: basketItem.foodName,
         shop: basketItem.shop,
         price: basketItem.price,
@@ -60,6 +62,26 @@ class Basket with ChangeNotifier {
         foodOrSnacks: basketItem.foodOrSnacks,
       ),
     );
+    notifyListeners();
+  }
+
+  //TOTAL WORTH OF BASKET
+  int get totalBasketAmount {
+    var total = 0;
+
+    _basketItems.forEach((basketItems) {
+      total += int.parse(basketItems.price);
+    });
+    return total;
+  }
+
+  // DELETE AN ITEM FROM THE BASKET
+  void deleteItemFromBasket(String itemId) {
+    final existingStoryIndex =
+        _basketItems.indexWhere((element) => element.id == itemId);
+
+    _basketItems.removeAt(existingStoryIndex);
+
     notifyListeners();
   }
 }
