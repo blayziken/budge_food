37e6248e56cae7d9c578098b2714c60707c15e22
blayziken <kingtoluwalope@gmail.com>
2021-06-13 09:49:08 +0100

    more auth implementation

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProxyProvider<Auth, Orders>(
           update: (context, auth, previousOrders) => Orders(
             auth.token,
+            auth.userId,
             previousOrders == null ? [] : previousOrders.orders,
           ),
         ),
